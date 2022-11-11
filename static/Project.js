fa2ed660db5693dc74d8592607ba51b825b7d092
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2022-11-11 10:35:59 -0600

    initialize components and root

---
+++ static/Project.js
@@ -0,0 +1,30 @@
+const ProjectMap = () => {
+    const projects = [
+        {
+            id: 1,
+            title: 'Hack or Snooze',
+            image: 'Implement Dark Mode',
+            url: 'https://cassreinhart.github.io/hack-or-snooze/#',
+        },
+        {
+            id: 2,
+            title: 'imgman',
+            image: '',
+            video: 'Post this on youtube...',
+            url: 'https://imgman.herokuapp.com/',
+        },
+        {
+            id: 3,
+            title: 'Project Management App',
+            image: 'tbd',
+            url: 'tbd',
+        },
+        {
+            id: 4,
+            title: 'Boggle',
+            image: 'tbd',
+            video: 'Post me to youtube...',
+            url: 'link to github',
+        },
+    ]
+}
